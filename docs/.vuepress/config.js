0956e672fe45fe8d520147ba38bb8346727b9fcc
Rocher <rocher@126.com>
2018-05-24 16:00:11 +0800

    add dist in config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,11 @@
 module.exports = {
-    title: 'Soft Exam',
-    description: 'Just Pass Exam like Coding ‘Hello World’.',
-    themeConfig: {
-        nav: [
-          { text: 'Home', link: '/' },
-          { text: '系统分析师', link: '/SA/' },
-        ]
-      }
-  }
+  title: 'Soft Exam',
+  description: 'Just Pass Exam like Coding ‘Hello World’.',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: '系统分析师', link: '/SA/' },
+    ]
+  },
+  base: '/SoftExam/'
+}
