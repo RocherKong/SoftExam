e53e0159070de75435364d23883ae4324935bbfc
Rocher <rocher@126.com>
2018-05-24 15:01:21 +0800

    add soft exam

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    title: 'Soft Exam',
+    description: 'Just Pass Exam like Coding ‘Hello World’.',
+    themeConfig: {
+        nav: [
+          { text: 'Home', link: '/' },
+          { text: '系统分析师', link: '/SA/' },
+        ]
+      }
+  }
